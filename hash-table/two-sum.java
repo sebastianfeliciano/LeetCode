851fe870d75355219dcda858a560be270b1d668d
Sebastian Feliciano <115121233+sebastianfeliciano@users.noreply.github.com>
2024-06-27 21:33:17 -0700

    :ballot_box_with_check: two-sum [Runtime: 2 ms; Memory: 44.7 MB]

--- hash-table/two-sum.java
+++ hash-table/two-sum.java
@@ -1,16 +1,13 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> f = new HashMap<>();
-        for(int i = 0; i <= nums.length; i++){
-            int number = nums[i];
-            int difference = target - number;
-            if(f.containsKey(difference)){
-                return new int[] {f.get(difference), i};
-            } else {
-               f.put(number, i); 
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            int difference = target - nums[i];
+            if (map.containsKey(difference)) {
+                return new int[] { map.get(difference), i };
             }
+            map.put(nums[i], i);
         }
         return new int[]{};
-
     }
 }
