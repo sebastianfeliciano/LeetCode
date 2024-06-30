a22b47b5aa0686cde8984750051182c1740805b7
Sebastian Feliciano <115121233+sebastianfeliciano@users.noreply.github.com>
2024-06-29 19:17:04 -0700

    :ballot_box_with_check: two-sum [Runtime: 3 ms; Memory: 45 MB]

--- array/two-sum.java
+++ array/two-sum.java
@@ -3,8 +3,8 @@ class Solution {
         HashMap<Integer, Integer> map = new HashMap<>();
         for(int i = 0; i < nums.length; i++){
             int difference = target - nums[i];
-            if (map.containsKey(difference)) {
-                return new int[] { map.get(difference), i };
+            if(map.containsKey(difference)){
+                return new int[] { map.get(difference), i};
             }
             map.put(nums[i], i);
         }
