3cf8c64fa202a347d5970dd70ddb107f0ed6e271
Sebastian Feliciano <115121233+sebastianfeliciano@users.noreply.github.com>
2024-07-07 21:20:32 -0700

    :ballot_box_with_check: valid-palindrome [Runtime: 13 ms; Memory: 45 MB]

---
+++ string/valid-palindrome.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+        s = s.trim();
+        s = s.replaceAll("[^a-z0-9]", "");
+        if(s == null) return false;
+        int a_pointer = 0;
+        int b_pointer = s.length() - 1;
+
+        while(a_pointer < b_pointer){
+            if(s.charAt(a_pointer) != s.charAt(b_pointer)){
+               return false; 
+            }
+            a_pointer++;
+            b_pointer--;
+        }
+        return true;
+    }
+}
