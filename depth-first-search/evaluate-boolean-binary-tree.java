a5f5cf5400905f2ddf811cc6d4b0eef5482cc9f1
Sebastian Feliciano <115121233+sebastianfeliciano@users.noreply.github.com>
2024-07-07 23:06:52 -0700

    :ballot_box_with_check: evaluate-boolean-binary-tree [Runtime: 0 ms; Memory: 44.2 MB]

--- depth-first-search/evaluate-boolean-binary-tree.java
+++ depth-first-search/evaluate-boolean-binary-tree.java
@@ -18,17 +18,16 @@ class Solution {
         return helper(root);
     }
 
-    public boolean helper(TreeNode root){
-        if(root.val == 0 || root.val == 1) {
+    public boolean helper(TreeNode root) {
+        if(root.val == 0 || root.val == 1){
             return root.val == 1;
         }
-        if(root.val == 2) {
+        else if (root.val == 2){
             return helper(root.left) || helper(root.right);
         }
-        if(root.val == 3) {
+        else if (root.val == 3){
             return helper(root.left) && helper(root.right);
         }
         return false;
     }
-
 }
