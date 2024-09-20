34d6458e27c2beb70ed106ea8ef5336a68e9ad1f
Sebastian Feliciano <115121233+sebastianfeliciano@users.noreply.github.com>
2024-09-20 13:18:52 -0400

    LC-majority-element [Runtime: 13 ms; Memory: 49.2 MB]

--- hash-table/majority-element.java
+++ hash-table/majority-element.java
@@ -1,25 +1,41 @@
-import java.util.Map.Entry;
+import java.util.*;
 class Solution {
     public int majorityElement(int[] nums) {
-        Map<Integer, Integer> hashmap = new HashMap();
-        for(int number : nums){
-            if(hashmap.containsKey(number)){
-                hashmap.put(number, hashmap.get(number) + 1);
-            }
-            else {
-                hashmap.put(number, 1);
-            }
-        }
-        int highestCount = 0;
-        int numberPlaceHolder = -1;
-        for (Entry<Integer, Integer> entry : hashmap.entrySet()) {
-            if (entry.getValue() > highestCount) {
-                highestCount = entry.getValue();
-                numberPlaceHolder = entry.getKey();
-            }
-        }
-        return numberPlaceHolder;
+        HashMap<Integer, Integer> map = new HashMap<>();
 
+        for(int i = 0; i < nums.length; i++){
+            map.put(nums[i], map.getOrDefault(nums[i], 1) + 1);
+        }
 
-    }
+        int majorityCount = 1;  // Stores the highest count found         
+        int majorityElement = 0;  // Stores the element with the highest count   
+        
+        // Find the majority element
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {             
+            if (entry.getValue() != null) {  // Safeguard for potential nulls
+                int value = entry.getValue();             
+                if (value > majorityCount) {                 
+                    majorityCount = value;  // Update majority count                 
+                    majorityElement = entry.getKey();  // Update the majority element             
+                } 
+            }
+        }         
+        return majorityElement;     
+    } 
 }
+
+
+//         int majorityElement = 0;         
+//         int number = 0;          
+        
+//         // Find the majority element
+//         for (Map.Entry<Integer, Integer> entry : map.entrySet()) {             
+//             int value = entry.getValue();             
+//             if(value > majorityElement){                 
+//                 majorityElement = value;  // Update majority count                 
+//                 number = entry.getKey();  // Update the majority element             
+//             }         
+//         }         
+//         return number;   
+//     }
+// }
