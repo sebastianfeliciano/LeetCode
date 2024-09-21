fd5998bba26a59e54825b5c11ef7f6ea436597b1
Sebastian Feliciano <115121233+sebastianfeliciano@users.noreply.github.com>
2024-09-21 18:44:54 -0400

    LC-unique-number-of-occurrences [Runtime: 2 ms; Memory: 41.7 MB]

--- hash-table/unique-number-of-occurrences.java
+++ hash-table/unique-number-of-occurrences.java
@@ -5,6 +5,7 @@ class Solution {
         for(int i = 0; i < arr.length; i++){
             map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
         }
+        
         for(Map.Entry<Integer, Integer> entry : map.entrySet()){
             if(set.contains(entry.getValue())){
                 return false;
