4fd99380e0159c2b14ae3310b970e3f74aba6b91
Sebastian Feliciano <115121233+sebastianfeliciano@users.noreply.github.com>
2024-10-15 22:43:30 -0400

    LC-most-common-word [Runtime: 19 ms; Memory: 42.6 MB]

--- hash-table/most-common-word.java
+++ hash-table/most-common-word.java
@@ -1,27 +1,41 @@
+import java.util.*;
 class Solution {
     public String mostCommonWord(String paragraph, String[] banned) {
-        String newParagraph = paragraph.replaceAll("[^a-zA-Z]", " ").toLowerCase();
-        String[] listOfWords = newParagraph.split("\\s+");
-        Map<String, Integer> map = new HashMap<>();
-        for(int i = 0; i < listOfWords.length; i++){
-            map.put(listOfWords[i], map.getOrDefault(listOfWords[i], 0)+1);
-        }
 
-        for(int i = 0; i < banned.length; i++){
-            if(map.containsKey(banned[i])){
-                map.remove(banned[i]);
-            }
-        }
-
-        int mostCommonOccurances = 0;
         String word = "";
-        for(Map.Entry<String, Integer> entry : map.entrySet()){
-            if(entry.getValue() > mostCommonOccurances){
-                mostCommonOccurances = entry.getValue();
-                word = entry.getKey();
+        int freq = 0;
+
+
+
+        String motifiedParagraph = paragraph.toLowerCase().trim().replaceAll("[^a-z]", " ").replaceAll("\\s+", " ");
+
+        String[] listOfWords = motifiedParagraph.split(" ");
+        List<String> wordsList = new ArrayList<>(Arrays.asList(listOfWords));
+
+    for(int i = 0; i < banned.length; i++){
+        for(int j = 0; j < wordsList.size(); j++){
+            if(wordsList.get(j).equals(banned[i])){
+                wordsList.remove(j);
+                j--;
             }
         }
-        return word;
+    }
 
+    Map<String, Integer> map = new HashMap<>();
+
+    for(String name : wordsList){
+        map.put(name, map.getOrDefault(name, 0) + 1);
+    }
+
+    for(Map.Entry<String, Integer> entry : map.entrySet()){
+        if(entry.getValue() > freq){
+            freq = entry.getValue();
+            word = entry.getKey();
+        }
+    }
+
+
+
+        return word;
     }
 }
