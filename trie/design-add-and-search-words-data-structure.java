9e496e4d1c2518d72f7e15babc0d81391df9887f
Sebastian Feliciano <115121233+sebastianfeliciano@users.noreply.github.com>
2024-10-17 22:02:21 -0400

    LC-design-add-and-search-words-data-structure [Runtime: 170 ms; Memory: 89.4 MB]

--- trie/design-add-and-search-words-data-structure.java
+++ trie/design-add-and-search-words-data-structure.java
@@ -1,35 +1,57 @@
-public class WordDictionary {
-    private List<String> words;
+
+class WordDictionary {
+
+    private class TrieNode {
+        TrieNode[] child = new TrieNode[26];
+        boolean isWord;
+    }
+
+    TrieNode root;
 
     public WordDictionary() {
-        this.words = new ArrayList<>();
+        root = new TrieNode();
     }
-
+    
     public void addWord(String word) {
-        this.words.add(word);
-    }
-
-    public boolean search(String word) {
-        for (String w : this.words) {
-            if (match(w, word)) {
-                return true;
+        TrieNode temp = root;
+        char[] ar = word.toCharArray();
+        for(int i = 0; i < ar.length; i++){
+            if(temp.child[ar[i] - 'a'] == null){
+                temp.child[ar[i] - 'a'] = new TrieNode();
             }
+            temp = temp.child[ar[i]-'a'];
         }
-        return false;
+        temp.isWord = true;
     }
 
-    private boolean match(String w, String word) {
-        if (w.length() != word.length()) {
-            return false;
-        }
-        for (int i = 0; i < w.length(); i++) {
-            if (word.charAt(i) != '.' && word.charAt(i) != w.charAt(i)) {
+    boolean helper(TrieNode node, int pos, char[] ch){
+        for(int i = pos; i < ch.length; i++){
+            if(ch[i] == '.'){
+                for(int p = 0; p < 26; p++){
+                    if(node.child[p]!=null){
+                        if(helper(node.child[p], i+1, ch)){
+                            return true;
+                        }
+                    }
+                }
                 return false;
+            } else {
+                if(node.child[ch[i] - 'a'] != null){
+                    node = node.child[ch[i] - 'a'];
+                } else {
+                    return false;
+                }
             }
         }
-        return true;
+        return node.isWord;
+    }
+    
+    public boolean search(String word) {
+        char[] c = word.toCharArray();
+        return helper(root, 0, c);
     }
 }
+
 /**
  * Your WordDictionary object will be instantiated and called as such:
  * WordDictionary obj = new WordDictionary();
